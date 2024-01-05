804b60e91d9720edf9d46805b346b02172073001
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-05 05:34:20 +0300

    Task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Contains Pascal Triangle generator function"""
+from math import factorial as fact
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal's triangle of `n`."""
+
+    if n <= 0:
+        return []
+    pascal_triangle = []
+    for row in range(n + 1):
+        row_list = []
+        for k in range(row + 1):
+            row_list.append(int(fact(row) / (fact(k) * fact(row - k))))
+        pascal_triangle.append(row_list)
+
+    return (pascal_triangle)
+
+
+# based on Pascal’s Triangle Binomial Expansion
+"""
+Pascal's triangle defines the coefficients which appear in binomial expansions.
+That means the nth row of Pascal's triangle comprises the coefficients of the
+expanded expression of the polynomial (x + y)^n.
+
+The expansion of (x + y)^n is:
+(x + y)^n = a0x^n + a1x^(n-1)y + a2x^(n-2)y^2 + … + an-1xy^(n-1) + any^n
+
+where the coefficients of the form ak (a0, a1 ... an) are precisely the numbers
+in the nth row of Pascal's triangle. This can be expressed as:
+    ak = (n k) = n!/(k! * (n-k)!)
+"""
