2f3cbf41aee6ed47a1d07557c589cf66b80e81c5
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-12 03:09:09 +0300

    Clean up

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,41 +14,38 @@ def canUnlockAll(boxes: 'list[list]') -> bool:
     """
     if len(boxes) == 0:
         return True
+
     # a list containg a set of opened boxes & a set of acquired keys
     queue = [{0}, set(k for k in boxes[0] if k != 0 and k < len(boxes))]
 
-    # separate set of keys found. used to stop loop if all keys are found
+    # Set of keys found. Used to stop loop if all keys are found
     keys_found = {0}
     keys_found.update(boxes[0])
 
-    boxes_len = len(boxes)
-    # all_keys = {k for k in range(boxes_len)}
-    # loop as long as all boxes are not opened & there is unused key
-    while len(queue[0]) != boxes_len and queue[1]:
-        # print(f'keys_found:{keys_found}\nqueue:{queue}')
+    number_of_boxes = len(boxes)
 
-        # if all keys are found return
-        if len(keys_found) == boxes_len:
+    # loop as long as all boxes are not opened & there is unused key
+    while len(queue[0]) != number_of_boxes and queue[1]:
+
+        # if all keys are found return True
+        if len(keys_found) == number_of_boxes:
             return True
 
         # get nxt box key and remove key from set of keys in queue
         next_box = queue[1].pop()
-        # print(f'next_box: {next_box}')
+
         # add box to be opened (next_box) to set of opened boxes in queue
         queue[0].add(next_box)
 
         # open box and take keys of boxes not visited sofar (not in queue[0])
-        # new_keys = set(boxes[next_box]).difference(queue[0])
-        new_keys = {
-            k for k in boxes[next_box] if k < boxes_len
-            }.difference(queue[0])
-        # print(f'new keys: {new_keys}')
+        new_keys = {k for k in boxes[next_box] if k < number_of_boxes}
+        new_keys.difference_update(queue[0])
 
-        # add new keys to set of keys in queue and to set of keys found
+        # add new keys to set of keys in queue, and also to set of keys found
         queue[1].update(new_keys)
         keys_found.update(new_keys)
 
-    # if all boxes
-    if len(queue[0]) == boxes_len:
+    # if all boxes are opened return True
+    if len(queue[0]) == number_of_boxes:
         return True
     return False
