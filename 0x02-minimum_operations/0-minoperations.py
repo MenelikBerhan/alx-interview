548cce7106be08f2ad5d4cb84ac2378a7a86ac72
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-17 00:21:02 +0300

    Remove optimizing dict

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,29 +2,21 @@
 """Contains a function to solve Minimum Operations puzzle"""
 
 
-def minOperations(n: int, operations_dict: 'dict[int, int]' = {}) -> int:
+def minOperations(n: int) -> int:
     """Given a text file with a single character `H`, and a text editor that
     can execute only two operations in this file: `Copy All` and `Paste`,
     calculates the fewest number of operations needed to result in exactly
     `n` number of `H` characters in the file.
 
     Args:
-        n: number of `H` characters in the resulting text file.
-        operations_dict: dictionary to store already found minOperations
-            from previous calls.
+        n (int): number of `H` characters in the resulting text file.
 
     Returns:
-        int: fewest number of operations needed to result in exactly
-            `n` number of `H` characters in the file. If n is impossible
+        int: fewest number of operations needed. If n is impossible
             to achieve, returns `0`.
     """
-    # print(f'Start: n={n}, dict: {operations_dict}')
     if type(n) != int or n <= 1:
         return 0
-
-    if n in operations_dict:
-        # print(f'returned from dict for n:{n}, {operations_dict}')
-        return operations_dict[n]
 
     """ if n = k * m for integers k & m, then to reach n, first: reach k,
     then do: one `copy all` and (m - 1) times `paste`. (k + (m - 1)*k = km = n)
@@ -39,13 +31,8 @@ def minOperations(n: int, operations_dict: 'dict[int, int]' = {}) -> int:
             break
         k -= 1
 
-    # print(f'after loop: n={n}, kmax={k_max}')
-    if k != 1 and n % k == 0:
-        minOperations_k = minOperations(k, operations_dict)
-        operations_dict[n] = minOperations_k + n // k
-        return (minOperations_k + n // k)
+    if k != 1:   # n = k * (n//k) & minOperations(n) = minOperations(k) + n//k
+        return (minOperations(k) + n // k)
 
     # n is a prime number, miOperations(n) = miOperations(1) + n = n
-    operations_dict[n] = n
-    # print(f'END: at n={n}, {operations_dict}')
     return (n)
