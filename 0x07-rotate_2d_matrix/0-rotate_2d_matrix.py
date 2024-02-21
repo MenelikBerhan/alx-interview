ec0a17c65130bb26c2c202902b60bdd8c625b0d8
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-21 22:53:37 +0300

    Use string to type annotate

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Rotate 2D Matrix Inplace
 """
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List]) -> None:
+def rotate_2d_matrix(matrix: 'list[list]') -> None:
     """Rotates an `n x n` matrix 90 degrees clockwise and inplace,
     by first transposing the matrix, then reversing each row."""
     n = len(matrix)
